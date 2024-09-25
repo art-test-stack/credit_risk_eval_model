9a07d69d16bbddea85049320d82b5ab592dd7c4e
Arthur Testard <testardarthur@gmail.com>
2024-09-25 14:54:21 +0200

    pipeline

---
+++ src/pipeline/pipeline.py
@@ -0,0 +1,21 @@
+from src.data.preprocess import preprocess_data
+from src.data.corenlp import CoreNLP
+from src.data.glove import GloVe
+
+from src.model.model import CREModel
+
+from src.eval.metric import GMean, AUC
+
+import pandas as pd
+from typing import Any
+from pathlib import Path
+
+
+def pipeline(
+        loans_file: Path | str = Path("data/accepted_2007_to_2018Q4.csv"),
+        model: CREModel = CREModel(),
+        seg_model: CoreNLP = CoreNLP(),
+        emb_model: GloVe = GloVe(),
+    ) -> Any:
+    
+    df = preprocess_data(loans_file)
