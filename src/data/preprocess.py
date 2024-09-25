a7ff91703af9aaeb31d8b9a73405d3c3302f4e29
Arthur Testard <testardarthur@gmail.com>
2024-09-25 15:20:24 +0200

    normalization + optional dev set split

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -5,12 +5,13 @@ from src.data.glove import GloVe
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 import math
 
 import torch
 from pathlib import Path
-from typing import Union
+from typing import Union, Tuple
 
 
 
@@ -51,12 +52,13 @@ def preprocess_textual_feature(
         emb_model: GloVe = GloVe()
     ) -> torch.Tensor:
 
-    raw = df.copy()
-    desc = raw["desc"].values
+    desc = df["desc"].values
+    df.drop(columns="desc", inplace=True)
 
     # PREPROCESS HERE
     segmented_text = seg_model(desc)
     embeddings = emb_model(segmented_text, to_tensor=True) # B, S, D
+    # pad all loan textual descriptions to include 200 terms for loan descriptions from LendingClub
 
     return embeddings
 
@@ -66,30 +68,55 @@ def split_data(df: pd.DataFrame, get_dev_set: bool = False) -> pd.DataFrame:
     ftrs = list(features.copy().keys())
     ftrs.remove("loan_status")
     X, y = df[ftrs], df[["loan_status"]]
+
     if get_dev_set:
         X_train, X_devtest, y_train, y_devtest = train_test_split(X, y, test_size=.2, shuffle=False)
         X_dev, X_test, y_dev, y_test = train_test_split(X_devtest, y_devtest, test_size=.5, shuffle=False)
+
     else:
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, shuffle=False)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, shuffle=False)
 
     return X_train, X_dev, X_test, y_train, y_dev, y_test
 
 
-def normalize(X_train, X_dev, X_test, y_train, y_dev, y_test) -> None:
-    pass
+def normalize(
+        X_train: pd.DataFrame, 
+        X_test: pd.DataFrame, 
+        X_dev: pd.DataFrame | None = None,
+        scaler: StandardScaler = StandardScaler(),
+        return_scaler: bool = False
+    ) -> None:
+
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    if X_dev:
+        X_dev = scaler.transform(X_dev)
+
+    if return_scaler:
+        X_train, X_test, X_dev, scaler
+    return X_train, X_test, X_dev
 
 
-def preprocess_data(file_path: Union[str, Path] = "data/accepted_2007_to_2018Q4.csv", get_dev_set: bool = False) -> pd.DataFrame:
+def preprocess_data(
+        file_path: Union[str, Path] = Path("data/accepted_2007_to_2018Q4.csv"), 
+        get_dev_set: bool = False,
+        seg_model: CoreNLP = CoreNLP(), 
+        emb_model: GloVe = GloVe()
+    ) -> pd.DataFrame:
     df = pd.read_csv(file_path)
 
     df = select_features(df)
 
     hard = preprocess_hard_features(df)
-    desc = preprocess_textual_feature(df)
 
     X_train, X_dev, X_test, y_train, y_dev, y_test = split_data(df, get_dev_set)
 
-    X_train, X_dev, X_test, y_train, y_dev, y_test = normalize(X_train, X_dev, X_test, y_train, y_dev, y_test)
+    train_desc = preprocess_textual_feature(X_train)
+    if get_dev_set:
+        dev_desc = preprocess_textual_feature(X_dev)
+    test_desc = preprocess_textual_feature(X_train)
 
-    return X_train, X_dev, X_test, y_train, y_dev, y_test 
+    X_train, X_test, X_dev = normalize(X_train, X_test, X_dev)
+    # TO TENSOR
+    return (X_train, train_desc, y_train), (X_test, test_desc, y_test), (X_dev, dev_desc, y_dev)
 
